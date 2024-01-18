857e451f79d2d3750fc2dc327932c0d85a432ef8
Christian magallanes <desarrollowebfront@gmail.com>
2024-01-18 14:25:11 -0300

    hola

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
             <div className='header-text'>
                 <h1 className='titulo'>Arte en Enmarcados</h1>
                 <p className='header-p'>Transforma tus recuerdos en arte único. Fotos personalizadas en portarretratos, diseñadas para ti.</p>
-                <button className='header-btn'>comprame<FaAngleDown /></button>
+                <button className='header-btn'><FaAngleDown /></button>
             </div>
             <div className='di-container'>
         <img className='di' src={miDiseño} alt="a"/>
